b863c981a03bc4c246244c049b1382769585a8c9
elviravdiji0 <elviravdiji0@gmail.com>
2026-02-03 13:33:09 +0100

    partial design

--- src/utils/sidebar.ts
+++ src/utils/sidebar.ts
@@ -1,12 +1,24 @@
 import type { SidebarEntry } from "node_modules/@astrojs/starlight/utils/routing/types";
 
 export const checkIsCurrent = (entries: SidebarEntry[]): boolean => {
-    return entries.some(entry => entry.type === "link" ? entry.isCurrent : checkIsCurrent(entry.entries))
+  return entries.some(entry => entry.type === "link" ? entry.isCurrent : checkIsCurrent(entry.entries))
+}
+
+export const getCurrent = (entries: SidebarEntry[]): SidebarEntry[] => {
+  for (const entry of entries) {
+    if (entry.type === 'link' && entry.isCurrent) return [entry];
+    else if (entry.type === "group") {
+      for (const subEntry of entry.entries) {
+        if (subEntry.type === "link" && subEntry.isCurrent) return [entry, subEntry];
+      }
+    }
+  }
+  return [];
 }
 
 export const slugify = (...names: (string | false | undefined | null)[]) => {
   return names.filter(s => !!(typeof s === "string" ? s.trim() : s))
-  .join("-")
+    .join("-")
     .normalize("NFD")                  // split accented letters
     .replace(/[\u0300-\u036f]/g, "")   // remove accents
     .toLowerCase()
