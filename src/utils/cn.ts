1f120f2ed1da82d72d07d30e7b3cac7fe43c67b5
Isni Shabani <isnishabani.95@gmail.com>
2026-03-03 14:25:48 +0100

    Small fixes

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,4 +1,6 @@
 import { twMerge } from "tailwind-merge";
-import clsx, {type ClassValue} from "clsx";
+import clsx, { type ClassValue } from "clsx";
 
-export default function cn(...classes: ClassValue[]) {return twMerge(clsx(classes)) }
+export default function cn(...classes: ClassValue[]) {
+  return twMerge(clsx(classes));
+}
